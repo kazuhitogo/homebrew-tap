d76dea3ba5cef5d81c3a774c9366fdd85f93bf30
Kazuhito Go <kazuhitogo@gmail.com>
2026-05-01 21:53:50 +0900

    chore: bump Peekr to v1.1.0

--- Casks/peekr.rb
+++ Casks/peekr.rb
@@ -1,8 +1,8 @@
 cask "peekr" do
-  version "1.0.0"
-  sha256 "75bd800ad873cd76d269fd77989cfe62465ec9de9f6cb842b89ce1e255af7e2b"
+  version "1.1.0"
+  sha256 "79e1e7d47198c4819e0a627de5f0c0289aa2ca792a6afbf9a28f64577f2110ba"
 
-  url "https://github.com/kazuhitogo/mac-peekr/releases/download/v#{version}/Peekr.zip"
+  url "https://github.com/kazuhitogo/mac-peekr/releases/download/v#{version}/Peekr_v#{version}.zip"
   name "Peekr"
   desc "macOS system monitor floating widget"
   homepage "https://github.com/kazuhitogo/mac-peekr"
