1a3d81a09e3b059f624b43b17255f1436c0720b9
Kazuhito Go <kazuhitogo@gmail.com>
2026-05-09 16:35:51 +0900

    chore: bump Peekr to v1.8.0

--- Casks/peekr.rb
+++ Casks/peekr.rb
@@ -1,6 +1,6 @@
 cask "peekr" do
-  version "1.7.0"
-  sha256 "914a15cbfbf3dbeec35bcde2476bf08fa54706e2616e2bc79a4b27908406ff32"
+  version "1.8.0"
+  sha256 "f95c51da85e4f0156d6bc5e21e1668c79869658dd337d536ab03ba802d5b7392"
 
   url "https://github.com/kazuhitogo/mac-peekr/releases/download/v#{version}/Peekr_v#{version}.zip"
   name "Peekr"
